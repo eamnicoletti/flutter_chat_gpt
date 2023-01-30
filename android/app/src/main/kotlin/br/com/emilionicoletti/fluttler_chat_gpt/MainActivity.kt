73c23bc91cfff50a2ccdb8db6102211410488dac
Emílio Nicoletti <eamnicoletti@gmail.com>
2023-01-30 15:52:02 -0300

    create project

---
+++ android/app/src/main/kotlin/br/com/emilionicoletti/fluttler_chat_gpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.emilionicoletti.fluttler_chat_gpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/app_config.dart
@@ -0,0 +1,6 @@
+class AppConfig {
+  AppConfig._();
+
+  static String get getOpenAIAPIKey =>
+      "P2XnhfXnb75spAzLicenT3BlbkFJECyh2CJXNkcP4aediTOy";
+}
